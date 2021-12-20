cd27d3237e6b175ac77f2faf63123f6bec8c91db
AlexU <alexunderkorje@>
2021-12-20 03:04:05 +0300

    Initial commit

---
+++ src/PingPong.java
@@ -0,0 +1,22 @@
+public class PingPong extends Thread {
+	private boolean f;
+	public PingPong(boolean f) {
+		this.f=f;
+	}
+	@Override
+	public void run() {
+		if (f) System.out.println("Ping");
+		else System.out.println("Pong");
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		while (true){
+			PingPong thread1 = new PingPong(true);
+			PingPong thread2 = new PingPong(false);
+			thread1.start();
+			PingPong.sleep(1000);
+			thread2.start();
+			PingPong.sleep(1000);
+		}
+	}
+}
